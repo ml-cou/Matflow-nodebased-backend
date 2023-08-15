a37e86ccec7a92a41856c899671c7c945de63ca2
Md Hasan Hafizur Rahman <123562825+ml-cou@users.noreply.github.com>
2023-08-15 22:21:16 +0600

    Add files via upload

--- Matflow/urls.py
+++ Matflow/urls.py
@@ -17,17 +17,17 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from matflow_test.views import login, signup, display_group, test_page, display_correlation, eda_barplot, \
+from matflow_test.views import login, signup, display_group, display_correlation, eda_barplot, \
     display_correlation_featurePair, eda_pieplot, eda_boxplot, eda_countplot, eda_histogram, eda_violinplot, \
     eda_scatterplot, eda_regplot, eda_lineplot, eda_customplot, feature_creation, Alter_field, changeDtype, \
     merge_dataset, Encoding, Scaling, Drop_row, Drop_column, Append, Cluster, Split, Time_series, Time_series_analysis, \
-    Hyper_opti, Build_model, model_evaluation, Reverse_ml, model_prediction, display_correlation_heatmap, download_model
+    Hyper_opti, Build_model, model_evaluation, Reverse_ml, model_prediction, display_correlation_heatmap, \
+    download_model, deploy_data, deploy_result
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/signup/', signup, name='signup'),
     path('api/login/', login, name='login'),
-    path('',test_page),
     path('api/display_group/', display_group, name='display-api'),
     path('api/display_correlation/', display_correlation, name='display-api'),
     path('api/display_correlation_featurePair/', display_correlation_featurePair, name='display-api'),
@@ -61,5 +61,7 @@ urlpatterns = [
     path('api/model_prediction/', model_prediction, name='dropping'),
     path('api/download_model/', download_model, name='download_model'),
     path('api/reverseml/', Reverse_ml, name='dropping'),
+    path('api/deploy_data/',deploy_data,name="deploy_data"),
+    path('api/deploy_result/', deploy_result, name="deploy_result")
 
 ]
